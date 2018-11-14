4bdf2acaf8b8e50b37443bdb8b3e2f92076382f6
Alexander A. Kovalenko <zingorn@gmail.com>
2018-11-15 00:29:25 +0200

    Add Travis CI support

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,8 +6,7 @@ if [ -d ${OPEN_VINO_DIR}/bin ]; then
 fi
 
 echo "Download archives..."
-# Note: the following link valid only for 2018.3.343 version
-wget -O $HOME/openvino.tgz http://registrationcenter-download.intel.com/akdlm/irc_nas/13521/l_openvino_toolkit_p_${OPENVINO_VERSION}.tgz
+wget -O $HOME/openvino.tgz http://registrationcenter-download.intel.com/akdlm/irc_nas/${OPENVINO_DOWNLOAD_ID}/l_openvino_toolkit_p_${OPENVINO_VERSION}.tgz
 tar -xf $HOME/openvino.tgz -C $HOME
 
 echo "Installing the OpenVino dependencies..."
